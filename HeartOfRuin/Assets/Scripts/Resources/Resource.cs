2ffdc37c21f041bc5a3833ee1edac8beb0f4c33c
callumstreets21 <149797713+callumstreets21@users.noreply.github.com>
2025-10-28 14:09:33 +0000

    Guard added to prevent negative input values into the resource base class
    Negative values simply trigger a return to prevent them affecting resource.

--- HeartOfRuin/Assets/Scripts/Resources/Resource.cs
+++ HeartOfRuin/Assets/Scripts/Resources/Resource.cs
@@ -25,12 +25,14 @@ public class Resource : MonoBehaviour
     protected void increaseResource(float amount)
     {
         if (currentResource >= maxResource) return;
+        if (amount <= 0) return;
         currentResource = Mathf.Min(currentResource + amount, maxResource);
     }
 
     protected void decreaseResource(float amount)
     {
         if (currentResource <= 0) return;
+        if (amount <= 0) return;
         currentResource = Mathf.Max(currentResource - amount, 0);
     }
 
