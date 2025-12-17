1853964f9abc0ef778eeb4a5fbb1acb8a5ee696e
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2025-12-17 02:12:10 +0000

    clean up files

--- HeartOfRuin/Assets/Settings/UnusedAssetConfiguration.cs
+++ HeartOfRuin/Assets/Settings/UnusedAssetConfiguration.cs
@@ -8,8 +8,9 @@ public class UnusedAssetConfiguration : IUnusedAssetConfiguration
         assetPaths.RemoveAll(path => path.StartsWith("Assets/Plugins/"));
         assetPaths.RemoveAll(path => path.StartsWith("Assets/_2ndParty/"));
         assetPaths.RemoveAll(path => path.StartsWith("Assets/_3rdParty/"));
-        assetPaths.RemoveAll(path => path.StartsWith("Assets/_Scenes/Debug Scennes/"));
+        assetPaths.RemoveAll(path => path.StartsWith("Assets/_Scenes/Debug Scenes/"));
         assetPaths.RemoveAll(path => path.StartsWith("Assets/_Scenes/Demo/"));
         assetPaths.RemoveAll(path => path.Contains("credits.txt"));
+        assetPaths.RemoveAll(path => path.Contains("Assets/Shared"));
     }
 }
