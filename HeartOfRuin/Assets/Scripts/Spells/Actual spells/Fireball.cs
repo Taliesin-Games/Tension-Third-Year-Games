c8bb20ea2d5a4954ba8a5f7f47d0743159a3a582
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-05-22 00:03:23 +0100

    Fixed Enemies not firing projectiles

--- HeartOfRuin/Assets/Scripts/Spells/Actual spells/Fireball.cs
+++ HeartOfRuin/Assets/Scripts/Spells/Actual spells/Fireball.cs
@@ -17,7 +17,36 @@ public class Fireball : SpellBase
             context.CastOrigin,
             Quaternion.LookRotation(context.Direction));
 
+        // If layer is player set layer to PlayerProjectile, if layer is enemy set layer to EnemyProjectile, otherwise default to Projectile
+        int layer;
+
+        if (context.Caster.layer == LayerMask.NameToLayer("Player"))
+        {
+            layer = LayerMask.NameToLayer("PlayerProjectiles");
+        }
+        else if (context.Caster.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            layer = LayerMask.NameToLayer("EnemyProjectiles");
+        }
+        else
+        {
+            layer = LayerMask.NameToLayer("Projectiles");
+        }
+
+        SetLayerRecursively(projectile, layer);
+
+
         projectile.GetComponent<Projectile>()?.SetSpell(this); 
+    }
+
+    private void SetLayerRecursively(GameObject obj, int layer)
+    {
+        obj.layer = layer;
+
+        foreach (Transform child in obj.transform)
+        {
+            SetLayerRecursively(child.gameObject, layer);
+        }
     }
 
     public override void DealDamage(GameObject target)
